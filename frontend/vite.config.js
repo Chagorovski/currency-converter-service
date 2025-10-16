4f2606a824f97afcbe124eb716faec3c1422e352
Vladimir Chagorovski <vladimir.chagorovski@valtech.com>
2025-10-16 20:33:26 +0200

    Update vite.config and README.md

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,33 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    watch: { usePolling: true },
-    hmr: { clientPort: 5173 },
-    proxy: {
-      '/api': {
-        target: 'http://nginx',
-        changeOrigin: true
+export default ({ command, mode }) => {
+  const isBuild = command === 'build'
+
+  return defineConfig({
+    base: isBuild ? '/app/' : '/',
+
+    plugins: [vue()],
+
+    server: {
+      host: true,
+      port: 5173,
+      strictPort: true,
+      watch: { usePolling: true },
+      hmr: { clientPort: 5173 },
+      proxy: {
+        '/api': {
+          target: 'http://nginx',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-})
+
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    },
+  })
+}
